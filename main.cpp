8df0b46fd3412fc4c67f6fdbb4f6bc3e973ecd62
swilfredojoel <swilfredojoel@gmail.com>
2024-06-24 23:15:51 -0300

    main created and running

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+#include <Graph.h>
+#include <Reader.h>
+#include <stdlib.h>
+
 int main()
 {
+  Reader read;
+  read.run();
 
+  vector<map<char, vector<char>>> graphs = read.get_input();
 
-    return 0;
+  for (const auto graph : graphs)
+  {
+    Graph g(graph);
+    g.show_graph();
+    for (int i=0; i<100; i++) cout << "-";
+    cout << "\n\n";
+  }
+
+  _flushall();
+
+  system("pause");
+
+  return 0;
 }
